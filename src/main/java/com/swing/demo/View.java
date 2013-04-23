d382aac7d481e2a38f7a10afb628bcdf14c76e64
Stephen Neal <stephen.neal1@gmail.com>
2013-04-23 12:36:16 +0930

    Testing combo box binding with ObservableList

--- src/main/java/com/swing/demo/View.java
+++ src/main/java/com/swing/demo/View.java
@@ -6,6 +6,9 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -13,6 +16,8 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import org.jdesktop.observablecollections.ObservableCollections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.factories.Borders;
@@ -23,6 +28,8 @@ import com.swing.plus.mvc.DualModePresentationModel.ViewMode;
 
 @SuppressWarnings("serial")
 public class View extends JPanel {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(View.class);
 
     public static void main(String[] args) {
         final BindingService binding = new BindingService();
@@ -58,7 +65,7 @@ public class View extends JPanel {
         PersonPanel personPanel = new PersonPanel(pModel, bindingService);
         setDefaultBorder(personPanel, personPanel.getModel().getTitle());
 
-        AddressModel aModel = new AddressModel();
+        final AddressModel aModel = new AddressModel();
         aModel.setAddress1("35 Runway Drive");
         // aModel.setAddress2("Doo Dah");
         aModel.setPostcodes(ObservableCollections.observableList(getPostcodes()));
@@ -90,6 +97,25 @@ public class View extends JPanel {
         builder.append(addressPanel);
         builder.nextLine(2);
         builder.append(contactsPanel);
+
+        // Schedule some tasks to update combo boxes (they are properly updated)
+        final ScheduledExecutorService pool = Executors.newScheduledThreadPool(2);
+        pool.schedule(new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("changing states list");
+                aModel.setStates(ObservableCollections.observableList(getStates2()));
+            }
+        }, 5, TimeUnit.SECONDS);
+        pool.schedule(new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("adding postcodes");
+                aModel.getPostcodes().addAll(getPostcodes2());
+            }
+        }, 10, TimeUnit.SECONDS);
+        pool.shutdown();
+
         return panel;
     }
 
@@ -108,14 +134,32 @@ public class View extends JPanel {
         return postcodes;
     }
 
+    private static List<String> getPostcodes2() {
+        List<String> postcodes = new ArrayList<String>();
+        postcodes.add("5007");
+        postcodes.add("5008");
+        postcodes.add("5009");
+        postcodes.add("5010");
+        postcodes.add("5011");
+        postcodes.add("5012");
+        return postcodes;
+    }
+
     private static List<String> getStates() {
         List<String> states = new ArrayList<String>();
         states.add("SA");
         states.add("NSW");
-        states.add("VIC");
         states.add("QLD");
         states.add("TAS");
+        states.add("VIC");
         states.add("WA");
+        return states;
+    }
+
+    private static List<String> getStates2() {
+        List<String> states = new ArrayList<String>();
+        states.add("QLD");
+        states.add("VIC");
         return states;
     }
 }
